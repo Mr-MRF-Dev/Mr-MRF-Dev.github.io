43563aeae21f63890d803fb2ebf587ba7f42cff5
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2026-01-31 00:49:34 +0330

    style: update default theme preference to dark mode in theme toggle functionality

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -183,8 +183,8 @@ function updateStats(data) {
 const themeToggle = document.getElementById("themeToggle");
 const htmlElement = document.documentElement;
 
-// Check for saved theme preference or default to light mode
-const currentTheme = localStorage.getItem("theme") || "light";
+// Check for saved theme preference or default to dark mode
+const currentTheme = localStorage.getItem("theme") || "dark";
 htmlElement.setAttribute("data-theme", currentTheme);
 
 // Toggle theme
